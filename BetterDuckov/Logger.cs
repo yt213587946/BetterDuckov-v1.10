cb74e55407f4faf9604877a460f4602641d64327
Amoosekerrason <f130655046@gmail.com>
2025-11-13 18:34:23 +0800

    null...

--- BetterDuckov/Logger.cs
+++ BetterDuckov/Logger.cs
@@ -20,21 +20,21 @@ namespace bigInventory
             string sub = subMod != null ? $"[{subMod}]" : "";
             return $"[{DateTime.Now:HH:mm:ss}][{ModName}]{sub}: {message}";
         }
-        public static void Log(Level lvl, string message, string subMod)
+        public static void Log(Level lvl, string message, string subMod = null)
         {
             if (!ShouldLog(lvl)) return;
 
             Debug.Log(Format(message, subMod));
         }
 
-        public static void Error(Level lvl, string message, string subMod)
+        public static void Error(Level lvl, string message, string subMod = null)
         {
             if (!ShouldLog(lvl)) return;
 
             Debug.LogError("> !!! <" + Format(message, subMod));
         }
 
-        public static void Warn(Level lvl, string message, string subMod)
+        public static void Warn(Level lvl, string message, string subMod = null)
         {
             if (!ShouldLog(lvl)) return;
 
